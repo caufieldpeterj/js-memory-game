80d503d9714758df5dd2c2f6e86d0d2e74e3b723
peter caufield <caufield.peterj@gmail.com>
2021-06-01 15:33:45 -0400

    creaing grid, appending 4 cards to the grid to start, small typos prevented appendChild method from running properly.. be careful!

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoadded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     // card creation
     const cardArray = [
         {
-            name: '',
-            img: 'static/____.png'
+            name: '2C',
+            img: '/static/2C.png'
         },
         {
-            name: '',
-            img: 'static/____.png'
+            name: '2D',
+            img: '/static/2D.png'
         },
         {
             name: '',
@@ -22,25 +22,56 @@ document.addEventListener('DOMContentLoadded', () => {
     // create game board
 
     const grid = document.querySelector('.grid')
-    createBoard = () => {
+    let cardsChosen = [];
+    let cardsChosenId = [];
+    let cardsWon = [];
+
+    const createBoard = () => {
         for (let i=0;i<cardArray.length; i++) {
+            console.log(i);
             let card = document.createElement('img');
-            card.setAttribute('src', 'static/blank.png');
+            card.setAttribute('src', 'static/red_back.png');
+            card.setAttribute('id', 'card');
+            
             // give id from 0-cardArray.length
             card.setAttribute('data-id', i);
-            // card.addEventListener('click', flipcard)
+            card.addEventListener('click', flipCard)
             grid.appendChild(card);
         }
     }
 
     // check for matches
-    checkMatch = () => {
-
+    const checkMatch = () => {
+        let cards = document.querySelector('img');
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('Match!');
+            cards[optionOneId].setAttribute('src', 'static/blue_back.png');
+            cards[optionTwoId].setAttribute('src', 'static/blue_back.png');
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[optionOneId].setAttribute('src', './static/red_back.png');
+            cards[optionTwoId].setAttribute('src', './static/red_back.png');
+            alert('Sorry, no match');
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
     }
     
     // flip cards
 
-    flipCard = () => {}
-    
+    flipCard = () => {
+        let cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosenId.length === 2) {
+            setTimeout(checkMatch, 500);
+        }
+    }
+    createBoard();
+
 
 })
+
